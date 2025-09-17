d65b69cab4152ac321f75b87c48236ee78094627
Nihith303 <nihithjonnalagadda@gmail.com>
2025-09-17 16:23:02 +0530

    DS Arrays and Queue

---
+++ DataStructure/Arrays.ts
@@ -0,0 +1,10 @@
+let purlist = ['banana', 'mango', 'orange']
+console.log(purlist)
+purlist.push('pineapple') //add an element to the last
+console.log(purlist)
+purlist.unshift('grapes') //add element to the first
+console.log(purlist)
+purlist.pop() //remove the last element
+console.log(purlist)
+purlist.shift() //remove the first element
+console.log(purlist)
